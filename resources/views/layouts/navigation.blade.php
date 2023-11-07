a0258c1322ca51aff37e8777f13a224a21e9100b
febeFlo <steffanyflorence@gmail.com>
2023-11-07 21:20:18 +0700

    commit

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
+    <div class="container-fluid">
+    <div>
+        <a class="navbar-brand" href="#">
+        <img src="" alt="Paola" width="30" height="24">
+        </a>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Products</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+            </li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProdukController::class, 'products']);
 
 Route::get('/kategori/{kategori}', [KategoriController::class, 'show']);
