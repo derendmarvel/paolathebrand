dabc0e439bdabf62eb96a85768a10c6e5e588c09
febeFlo <steffanyflorence@gmail.com>
2023-12-23 15:37:08 +0700

    API

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -4,21 +4,19 @@
 
 @section('content')
     <strong>From :</strong>
-    <?php echo $from ?>
-    {{-- <?php echo $total->rajaongkir->origin_details->province ?> --}}
+    <?php echo $total->rajaongkir->origin_details->city ?>
 
     <strong>To :</strong>
-    <?php echo $to ?>
-    {{-- <?php echo $total->rajaongkir->destination_details->province ?> --}}
+    {{-- <?php echo $total->rajaongkir->destination_details->city ?> --}}
 
     <strong>Expedition :</strong>
-    <?php echo $total->rajaongkir->results[0]->name ?><br>
+    {{-- <?php echo $total->rajaongkir->results[0]->name ?><br> --}}
 
     <strong>Weight :</strong>
-    <?php echo $total->rajaongkir->query->weight/1000 ?>Kg<br>
+    {{-- <?php echo $total->rajaongkir->query->weight/1000 ?>Kg<br> --}}
 
     <strong>Cost :</strong>
-    @foreach ($total->rajaongkir->results[0]->costs as $biaya): ?><br>
-        <?php echo $biaya->service?> : <?php echo number_format($biaya->cost[0]->value) ?> (<?php echo $biaya->cost[0]->etd ?> Day)<br>
-    @endforeach
+    {{-- @foreach ($total->rajaongkir->results[0]->costs as $biaya): ?><br> --}}
+        {{-- <?php echo $biaya->service?> : <?php echo number_format($biaya->cost[0]->value) ?> (<?php echo $biaya->cost[0]->etd ?> Day)<br> --}}
+    {{-- @endforeach --}}
 @endsection
